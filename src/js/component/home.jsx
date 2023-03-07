5c39fda55766de850e0ca8252060d6e38f24dabf
DGgithubDG <thenumerousrandom@gmail.com>
2023-03-07 15:50:52 +0000

    w

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { FcApproval } from "react-icons/fc";
 import { FcBullish } from "react-icons/fc";
 import { FcLink } from "react-icons/fc";
-import casino from "../../img/casino.6.jpg";
+import casino from "../../img/casino.9.jpg";
 import Navbar from "./navbar";
 import Footer from "./footer";
 import Card from "./card";
@@ -17,17 +17,17 @@ const Home = () => {
     <Navbar/>
     <div></div><div>
     <img class="img-fluid" src={casino}></img>
-    <div class="card" >
-  <div class=" header list-group-item text-center">
-    <h3><strong><div className="header">KING J </div></strong></h3>
-  </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item text-center"><FcApproval/><i>Card text here</i></li>
-    <li class="list-group-item text-center"><FcBullish/><i>Card text here</i></li>
-    <li class="list-group-item text-center"><FcLink/><i>Card text here</i></li>
-  </ul>
-  <button className="btn btn-success text-center">REGISTER</button>
+    <div class="card text-center">
+  <div class="card-body">
+    <h5 class="card-title"><h1>CASINO</h1></h5>
+    <div class="mb-6"><hr class="solid"/> </div>
 
+    <p class="card-text"><FcApproval size={38}/><strong>Always the best odds with King J</strong></p>
+    <p class="card-text"><FcBullish size={38}/><strong>Quick and easy deposits & withdrawals</strong></p>
+    <p class="card-text"><FcLink size={38}/><strong>Transparency, responsibility and security</strong></p>
+   
+    <a href="#" class="button btn btn-success">REGISTER</a>
+  </div>
 </div>
     {/* <img className="Back" src="https://www.wildz.com/cdn/2021/09/30/wildz-blog-online-casino-2_uid_6155c3688c52d.jpeg?&cover=true" class="img-fluid" alt=""/>  */}
      {/* <Card/>  */}
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
 <div>
 		<nav class="navbar navbar-expand-lg navbar-light bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><h1><strong>KING J</strong></h1></a>
+    <a class="navbar-brand" href="#"><h1><strong>CASINO</strong></h1></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
